75b70256538fd17e6e47f09192b6dd5ea19a6a0c
anjmhrjn <anjmhrjn1@gmail.com>
2025-06-10 20:43:36 -0400

    trie medium problems

---
+++ trie/medium/add_search.py
@@ -0,0 +1,55 @@
+"""
+Design a data structure that supports adding new words and searching for existing words.
+Implement the WordDictionary class:
+void addWord(word) Adds word to the data structure.
+bool search(word) Returns true if there is any string in the data structure that matches word or false otherwise. word may contain dots '.' where dots can be matched with any letter.
+Example 1:
+Input:
+["WordDictionary", "addWord", "day", "addWord", "bay", "addWord", "may", "search", "say", "search", "day", "search", ".ay", "search", "b.."]
+Output:
+[null, null, null, null, false, true, true, true]
+Explanation:
+WordDictionary wordDictionary = new WordDictionary();
+wordDictionary.addWord("day");
+wordDictionary.addWord("bay");
+wordDictionary.addWord("may");
+wordDictionary.search("say"); // return false
+wordDictionary.search("day"); // return true
+wordDictionary.search(".ay"); // return true
+wordDictionary.search("b.."); // return true
+"""
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.word = False
+
+class WordDictionary:
+    def __init__(self):
+        self.root = TrieNode()        
+
+    def addWord(self, word: str) -> None:
+        cur = self.root
+        for c in word:
+            if not c in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.word = True        
+
+    def search(self, word: str) -> bool:
+
+        def dfs(j, root):
+            cur = root
+            for i in range(j, len(word)):
+                c = word[i] 
+
+                if c == ".":
+                    for child in cur.children.values():
+                        if dfs(i+1, child):
+                            return True
+                    return False
+                else:
+                    if c not in cur.children:
+                        return False
+                    cur = cur.children[c] 
+            return cur.word
+        return dfs(0, self.root)      
