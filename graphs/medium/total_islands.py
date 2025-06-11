9f19f80bdcf60c380c5214c0fed1960079c3719d
anjmhrjn <anjmhrjn1@gmail.com>
2025-06-10 23:09:12 -0400

    3 medium graph problems

---
+++ graphs/medium/total_islands.py
@@ -0,0 +1,42 @@
+"""
+Given a 2D grid grid where '1' represents land and '0' represents water, count and return the number of islands.
+An island is formed by connecting adjacent lands horizontally or vertically and is surrounded by water. You may assume water is surrounding the grid (i.e., all the edges are water).
+Example 1:
+Input: grid = [
+    ["0","1","1","1","0"],
+    ["0","1","0","1","0"],
+    ["1","1","0","0","0"],
+    ["0","0","0","0","0"]
+  ]
+Output: 1
+"""
+from collections import deque
+class Solution:
+    def numIslands(self, grid: list[list[str]]) -> int:
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        ROWS, COLS = len(grid), len(grid[0])
+        islands = 0
+
+        def bfs(r, c):
+            q = deque()
+            grid[r][c] = "0"
+            q.append((r, c))
+
+            while q:
+                row, col = q.popleft()  
+                for dr, dc in directions:
+                    nr, nc = dr + row, dc + col
+                    if (nr < 0 or nc < 0 or nr >= ROWS or
+                        nc >= COLS or grid[nr][nc] == "0"
+                    ):
+                        continue
+                    q.append((nr, nc))
+                    grid[nr][nc] = "0"
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == "1":
+                    bfs(r, c)
+                    islands += 1
+
+        return islands
